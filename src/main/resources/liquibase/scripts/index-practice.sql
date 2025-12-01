8c0331093a446dd78376c32f3971340863ac3be9
TimLvv <lavrentev.timur@mail.ru>
2025-12-01 20:55:54 +0300

    commit 10

--- src/main/resources/liquibase/scripts/index-practice.sql
+++ src/main/resources/liquibase/scripts/index-practice.sql
@@ -1,7 +1,10 @@
+
 -- liquibase formatted sql
+
 
 -- changeset tim:1
 CREATE INDEX students_name_index ON students(name);
+
 
 -- changeset tim:2
 CREATE INDEX faculties_name_index ON faculties(name);
